3f589879216d462c3ef49527501d1633a15a7ece
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-16 01:34:47 +0200

    Update player animations

--- Character/player.py
+++ Character/player.py
@@ -6,22 +6,22 @@ from Animation import AnimationController, Animation, SequenceAnimation, Transit
 class Player(Character):
     animation = AnimationController(
             animations=[
-                Animation('idle', "D:\\game assets\\zombie\\idle", speed=1.5),
-                Animation("walk", "D:\\game assets\\zombie\\walk", speed=1.5),
-                Animation("idle_to_walk", "D:\\game assets\\zombie\\idle to walk", speed=1.5),
+                Animation('idle', "D:\\game assets\\swat 1\\idle", speed=1.5),
+                Animation("walk", "D:\\game assets\\swat 1\\walk", speed=1.5),
+                Animation("turn", "D:\\game assets\\swat 1\\turn", speed=1.5),
                 SequenceAnimation(
-                    name="scream_run",
+                    name="turn_walk",
                     animations=[
-                        Animation("scream", "D:\\game assets\\zombie\\scream"),
-                        Animation("run", "D:\\game assets\\zombie\\run")
+                        Animation("turn", "D:\\game assets\\swat 1\\turn"),
+                        Animation("walk", "D:\\game assets\\swat 1\\walk")
                     ]
                 ),
-                Animation("attack", "D:\\game assets\\zombie\\attack", speed=1.5),
+                Animation("fire", "D:\\game assets\\swat 1\\fire", speed=1.5),
             ],
             default="idle",
-            transitions=[
-                TransitionRule("idle", "walk", "idle_to_walk", True),
-            ]
+            # transitions=[
+            #     TransitionRule("idle", "walk", "idle_to_walk", True),
+            # ]
         )
 
     def update(self):
@@ -30,18 +30,21 @@ class Player(Character):
     def event(self, event):
         if event.type == pygame.KEYDOWN:
             if event.key in (pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP, pygame.K_DOWN):
-                self.animation.switch_animation("walk")
                 self.moving = True
                 if event.key == pygame.K_RIGHT:
+                    self.animation.switch_animation("walk")
                     self.x_speed = self.speed[0]
                 elif event.key == pygame.K_LEFT:
+                    self.animation.switch_animation("turn_walk")
                     self.x_speed = -self.speed[0]
                 if event.key == pygame.K_UP:
+                    self.animation.switch_animation("walk")
                     self.y_speed = -self.speed[1]
                 elif event.key == pygame.K_DOWN:
+                    self.animation.switch_animation("walk")
                     self.y_speed = self.speed[1]
             if event.key == pygame.K_SPACE:
-                self.animation.switch_animation("attack", loop=False)
+                self.animation.switch_animation("fire", loop=False)
         elif event.type == pygame.KEYUP:
             if event.key in (pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP, pygame.K_DOWN):
                 self.animation.switch_animation("idle")
