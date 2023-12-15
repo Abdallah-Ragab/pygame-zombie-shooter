9e1acb2d2825c543d4094352c02a6c64ce6f45c2
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-16 01:18:52 +0200

    Update animation speeds and player movement in player.py

--- Character/player.py
+++ Character/player.py
@@ -6,9 +6,9 @@ from Animation import AnimationController, Animation, SequenceAnimation, Transit
 class Player(Character):
     animation = AnimationController(
             animations=[
-                Animation('idle', "D:\\game assets\\zombie\\idle"),
-                Animation("walk", "D:\\game assets\\zombie\\walk"),
-                Animation("idle_to_walk", "D:\\game assets\\zombie\\idle to walk"),
+                Animation('idle', "D:\\game assets\\zombie\\idle", speed=1.5),
+                Animation("walk", "D:\\game assets\\zombie\\walk", speed=1.5),
+                Animation("idle_to_walk", "D:\\game assets\\zombie\\idle to walk", speed=1.5),
                 SequenceAnimation(
                     name="scream_run",
                     animations=[
@@ -16,7 +16,7 @@ class Player(Character):
                         Animation("run", "D:\\game assets\\zombie\\run")
                     ]
                 ),
-                Animation("attack", "D:\\game assets\\zombie\\attack"),
+                Animation("attack", "D:\\game assets\\zombie\\attack", speed=1.5),
             ],
             default="idle",
             transitions=[
@@ -33,20 +33,20 @@ class Player(Character):
                 self.animation.switch_animation("walk")
                 self.moving = True
                 if event.key == pygame.K_RIGHT:
-                    self.speed = (1, 0)
+                    self.x_speed = self.speed[0]
                 elif event.key == pygame.K_LEFT:
-                    self.speed = (-1, 0)
+                    self.x_speed = -self.speed[0]
                 if event.key == pygame.K_UP:
-                    self.speed = (0, -1)
+                    self.y_speed = -self.speed[1]
                 elif event.key == pygame.K_DOWN:
-                    self.speed = (0, 1)
+                    self.y_speed = self.speed[1]
             if event.key == pygame.K_SPACE:
                 self.animation.switch_animation("attack", loop=False)
         elif event.type == pygame.KEYUP:
             if event.key in (pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP, pygame.K_DOWN):
                 self.animation.switch_animation("idle")
                 self.moving = False
-                self.speed = (0, 0)
+                self.x_speed, self.y_speed = 0, 0
 
         return super().event(event)
 
