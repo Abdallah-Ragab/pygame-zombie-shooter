bc0b9e5b48a8ac8a9655adf9982909910deb046b
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-16 01:18:45 +0200

    Add CameraAwareGroup and CameraAwareGroupSingle classes

---
+++ Character/group.py
@@ -0,0 +1,27 @@
+import pygame
+
+
+class CameraAwareGroup(pygame.sprite.Group):
+    def __init__(self, *sprites):
+        super().__init__(*sprites)
+        self.camera = None
+
+    def set_camera(self, camera):
+        self.camera = camera
+
+    def draw(self, surface):
+        for spr in self.sprites():
+            self.spritedict[spr] = surface.blit(spr.image, self.camera.apply(spr))
+        self.lostsprites = []
+
+class CameraAwareGroupSingle(pygame.sprite.GroupSingle):
+    def __init__(self, *sprites):
+        super().__init__(*sprites)
+        self.camera = None
+
+    def set_camera(self, camera):
+        self.camera = camera
+
+    def draw(self, surface):
+        for spr in self.sprites():
+            surface.blit(spr.image, self.camera.apply(spr))
