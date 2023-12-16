4c9481a064a41921050848912f9640cc76917686
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-16 22:40:26 +0200

    Scale cursor image to 40x40

--- cursor.py
+++ cursor.py
@@ -6,6 +6,7 @@ class Cursor:
     def __init__(self, player, min_distance=100, max_distance=700, max_angle=45):
         self.player = player
         self.image = pygame.image.load("assets/cursor.png")
+        self.image = pygame.transform.scale(self.image, (40, 40))
         self.rect = self.image.get_rect()
         self.rect.center = pygame.mouse.get_pos()
 
