b89de54892340382cf3230aa40b03fc1b2075ebe
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-16 22:49:36 +0200

    added camera angle switching based on player direction

--- camera.py
+++ camera.py
@@ -20,6 +20,8 @@ class Camera:
 
     def update(self, target):
         shooting_x_pos = self.width // 3
+        if target.direction == -1:
+            shooting_x_pos = self.width - shooting_x_pos
         target_x_offset = shooting_x_pos - target.rect.centerx
         reaction_x = min(0, target_x_offset)
         reaction_x = max(-(self.scene_width - self.width), reaction_x)  # right
