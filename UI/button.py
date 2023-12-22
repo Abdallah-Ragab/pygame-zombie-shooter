fd5bd5f5e9137d9c1d540b5b3b76b01335b6c58c
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-22 19:36:01 +0200

    Add support for hover images in Button class

--- UI/button.py
+++ UI/button.py
@@ -3,7 +3,26 @@ from .ui_element import UIElement
 
 
 class Button(UIElement):
+
+    normal_image = None
+    hover_image = None
+    normal_path = None
+    hover_path= None
+
     def __init__(self, x=0, y=0, image=None, path=None, right=None, width=None, height=None, scale=1, callback=None):
+        if isinstance(image, list):
+            self.normal_image = image[0]
+            self.hover_image = image[1]
+            image = self.normal_image
+        if isinstance(path, list):
+            self.normal_path = path[0]
+            self.hover_path = path[1]
+
+            if not self.hover_image:
+                self.hover_image = self.load_image(self.hover_path)
+
+            path = self.normal_path
+
         super().__init__(x, y, image, path, right, width, height, scale)
         self.callback = callback
 
@@ -26,3 +45,7 @@ class Button(UIElement):
         super().update()
         if self.is_clicked and self.callback:
             self.callback()
+        if self.is_hovered and self.hover_image:
+            self.image = self.hover_image
+        else:
+            self.image = self.normal_image
