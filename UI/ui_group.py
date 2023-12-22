bfb547435c3e8e42fa9d0749454db36cc11cf65e
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-23 00:24:33 +0200

    Add centering functionality to UIGroup class

--- UI/ui_group.py
+++ UI/ui_group.py
@@ -161,3 +161,31 @@ class UIGroup:
     def set_y(self, value):
         for element in self.elements:
             element.y = value
+
+    def center(self):
+        self.x = pygame.display.get_surface().get_width() / 2 - self.total_width / 2
+        self.y = pygame.display.get_surface().get_height() / 2 - self.total_height / 2
+        self.position()
+
+    @property
+    def rect(self):
+        elements = self.elements
+        if self.background:
+            elements.append(self.background)
+
+        xs = {element.x: element for element in elements}
+        ys = {element.y: element for element in elements}
+
+        max_x = max(xs.keys())
+        min_x = min(xs.keys())
+
+        max_y = max(ys.keys())
+        min_y = min(ys.keys())
+
+        width = max_x - min_x + xs[max_x].width + self.padding_x * 2
+        height = max_y - min_y + ys[max_y].height + self.padding_y * 2
+
+        x = min_x - self.padding_x
+        y = min_y - self.padding_y
+
+        return pygame.Rect(x, y, width, height)
