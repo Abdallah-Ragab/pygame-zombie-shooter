ff15ced2e3bc7a9794ba4060189e420bcd522c30
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-22 18:18:09 +0200

    Refactor Player class to improve organization and add new features

--- music.py
+++ music.py
@@ -1,22 +1,45 @@
+import random
 import pygame
 
 
-class Player():
-    queue = []
+class Player:
+    music_queue = []
+    background_effects_loop = []
     fadein_time = 500
     fadeout_time = 500
-    max_background_channels = 2
+    background_effects_channel_count = 2
+    background_music_channel_count = 2
+    background_effects_loop_running_channels = background_effects_channel_count
+    background_music_queue_running_channels = background_music_channel_count
 
-    def __init__(self, background_music: dict, sound_effects: dict):
+    def __init__(
+        self,
+        background_music: dict = None,
+        sound_effects: dict = None,
+        background_effects: dict = None,
+    ):
+        pygame.mixer.init()
+
         self.background_music = background_music
         self.sound_effects = sound_effects
+        self.background_effects = background_effects
 
-        pygame.mixer.set_reserved(self.max_background_channels)
-        self.background_channels = [pygame.mixer.Channel(x) for x in range(self.max_background_channels)]
+        pygame.mixer.set_reserved(
+            self.background_music_channel_count + self.background_effects_channel_count
+        )
 
-        for channel in self.background_channels:
+        self.background_music_channels = [
+            pygame.mixer.Channel(x) for x in range(self.background_music_channel_count)
+        ]
+        self.background_effects_channels = [
+            pygame.mixer.Channel(x)
+            for x in range(self.background_effects_channel_count)
+        ]
+
+        for channel in (
+            self.background_music_channels + self.background_effects_channels
+        ):
             channel.fadeout(self.fadeout_time)
-
 
     def add_background_music(self, background_music: dict):
         self.background_music.update(background_music)
@@ -24,54 +47,40 @@ class Player():
     def add_sound_effects(self, sound_effects: dict):
         self.sound_effects.update(sound_effects)
 
+    def add_background_effects(self, background_effects: dict):
+        self.background_effects.update(background_effects)
 
-    def play_background_music(self, music_name: str | list, loop: bool = True, volume: float = 1.0):
+    def play_background_music(
+        self, music_name: str | list, loop: bool = True, volume: float = 1.0
+    ):
         parallel_music = [music_name] if isinstance(music_name, str) else music_name
-        parallel_music = parallel_music[self.max_background_channels:]
+        parallel_music = parallel_music[self.background_music_channels_count :]
         for music in parallel_music:
-
             self._play_background_music(music, loop, volume)
 
-    def _play_background_music(self, music_name: str, loop: bool, volume: float):
-
-        if music_name not in self.background_music:
-            raise ValueError(f"{music_name} not found in background music")
-
-        self.queue.append((self.background_music[music_name], loop))
-
-        if not self.background_channels[0].get_busy():
-            self._play_next()
-
-    def enqueue_background_music(self, music_name: str | list, loop: bool = True, volume: float = 1.0):
+    def enqueue_background_music(
+        self, music_name: str | list, loop: bool = False, volume: float = 1.0
+    ):
         parallel_music = [music_name] if isinstance(music_name, str) else music_name
-        parallel_music = parallel_music[self.max_background_channels:]
+        parallel_music = parallel_music[self.background_music_channels_count :]
         for music in parallel_music:
             self._enqueue_background_music(music, loop, volume)
 
-    def _enqueue_background_music(self, music_name: str, loop: bool, volume: float):
-        if music_name not in self.background_music:
-            raise ValueError(f"{music_name} not found in background music")
+    def loop_background_effect(self, effects: list | str, channel_count=None):
+        if channel_count is not None:
+            self.background_effects_loop_running_channels = channel_count
+        else:
+            self.background_effects_loop_running_channels = self.background_effects_channel_count
 
-        self.queue.append((self.background_music[music_name], loop))
+        self.clear_background_effects_loop()
+        effects = [effects] if isinstance(effects, str) else effects
+        for effect_name in effects:
+            if effect_name not in self.background_effects:
+                raise ValueError(f"{effect_name} not found in background effects")
+            self.background_effects_loop.append(effect_name)
 
-    def _play_next(self):
-        music, loop = self.queue.pop(0)
-        self.background_channels[0].play(music, loop=loop, fade_ms=self.fadein_time)
-
-    def background_music_is_playing(self):
-        return any([channel.get_busy() for channel in self.background_channels])
-
-    def pause_background_music(self):
-        for channel in self.background_channels:
-            channel.pause()
-
-    def unpause_background_music(self):
-        for channel in self.background_channels:
-            channel.unpause()
-
-    def update(self):
-        if not self.background_music_is_playing() and self.queue:
-            self._play_next()
+    def clear_background_effects_loop(self):
+        self.background_effects_loop = []
 
     def play_sound_effect(self, sound_effect_name: str, volume: float = 1.0):
         if sound_effect_name not in self.sound_effects:
@@ -83,5 +92,75 @@ class Player():
         pygame.mixer.stop()
 
     def stop_background_music(self):
-        for channel in self.background_channels:
+        for channel in self.background_music_channels:
             channel.stop()
+
+    def play_background_effect(self, background_effect_name: str, volume: float = 1.0):
+        if background_effect_name not in self.background_effects:
+            raise ValueError(
+                f"{background_effect_name} not found in background effects"
+            )
+
+        pygame.mixer.find_channel().play(
+            self.background_effects[background_effect_name]
+        )
+
+    def pause_background_music(self):
+        for channel in self.background_music_channels:
+            channel.pause()
+
+    def unpause_background_music(self):
+        for channel in self.background_music_channels:
+            channel.unpause()
+
+    def _play_background_music(self, music_name: str, loop: bool, volume: float):
+        if music_name not in self.background_music:
+            raise ValueError(f"{music_name} not found in background music")
+
+        for channel in self.background_music_channels:
+            if not channel.get_busy():
+                channel.play(
+                    self.background_music[music_name],
+                    loop=loop,
+                    fade_ms=self.fadein_time,
+                )
+                return
+
+        self.background_music_channels[0].play(
+            self.background_music[music_name], loop=loop, fade_ms=self.fadein_time
+        )
+
+    def _enqueue_background_music(self, music_name: str, loop: bool, volume: float):
+        if music_name not in self.background_music:
+            raise ValueError(f"{music_name} not found in background music")
+
+        self.music_queue.append((self.background_music[music_name], loop))
+
+    def background_music_is_playing(self):
+        return any([channel.get_busy() for channel in self.background_music_channels])
+
+    def update(self):
+        self.run_music_queue()
+        self.run_effects_loop()
+
+    def run_effects_loop(self, ):
+        available_channels = [
+            channel
+            for channel in self.background_music_channels
+            if not channel.get_busy()
+        ][:self.background_effects_loop_running_channels]
+        for channel in available_channels:
+            if self.background_effects_loop:
+                effect = random.choice(self.background_effects_loop)
+                channel.play(self.background_effects[effect])
+
+    def run_music_queue(self):
+        available_channels = [
+            channel
+            for channel in self.background_music_channels
+            if not channel.get_busy()
+        ][:self.background_music_queue_running_channels]
+        for channel in available_channels:
+            if self.music_queue:
+                music = self.background_music[self.music_queue.pop(0)]
+                channel.play(music, fade_ms=self.fadein_time)
