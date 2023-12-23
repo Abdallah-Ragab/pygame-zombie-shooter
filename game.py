39b2b1f66c119da0f8b25d3e6bdf37512f138bdb
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-23 04:18:05 +0200

    Added docstrings

--- game.py
+++ game.py
@@ -1,22 +1,19 @@
-import json
-from typing import Any
 import pygame, sys
 
-# from pygamevideo import Video
 from Video.player import Video
 from director import Director, Scene
 
-from Character import Player, CameraAwareGroupSingle
-from camera import Camera
-from cursor import Cursor
 from music import Player as MusicPlayer
 
-# from hud import HUD
-from UI import HUD, Avatar, HealthBar, Ammo, Money, UIElement, UIGroup, Button
+from UI import UIGroup, Button
 from levels import Level
 
 
 class MenuScene(Scene):
+    """
+    A class representing a menu scene that inherits from the Scene class.
+    """
+
     music = MusicPlayer(
         background_music={
             "background": pygame.mixer.Sound("assets/sounds/main_menu_background.mp3")
@@ -27,18 +24,40 @@ class MenuScene(Scene):
     )
 
     def __init__(self, director):
+        """
+        Initialize an instance of the Game class with a director parameter.
+
+        Parameters:
+        director (Director): The director object.
+
+        Returns:
+        None
+        """
         super().__init__(director)
         self.music.play_background_music("background", loop=True)
 
-
     def update(self):
+        """
+        Update the menu and music components of the game.
+        @return None
+        """
         self.menu.update()
         self.music.update()
 
     def event(self, event):
+        """
+        Handle an event by passing it to the menu object's event handler.
+        @param event - the event to handle
+        """
         self.menu.event(event)
 
     def draw(self, screen, window_scale):
+        """
+        Draw the game screen on the given surface with the specified window scale.
+        @param screen - The surface to draw on.
+        @param window_scale - The scale factor for the window.
+        @return None
+        """
         screen.blit(
             pygame.transform.scale(
                 self.background, (self.director.width, self.director.height)
@@ -49,6 +68,10 @@ class MenuScene(Scene):
 
 
 class GamePlay(Level):
+    """
+    This is a class called `GamePlay` that inherits from `Level`. It has a `music` attribute which is an instance of `MusicPlayer`. The `MusicPlayer` is initialized with a dictionary of sound effects, where each sound effect is associated with a key. The sound effects are loaded from specific file paths.
+    """
+
     music = MusicPlayer(
         sound_effects={
             "shot": pygame.mixer.Sound("assets/sounds/shot.wav"),
@@ -80,6 +103,10 @@ class GamePlay(Level):
     )
 
     def setup(self):
+        """
+        Perform the setup for the game.
+        @return The result of the superclass's setup method.
+        """
         self.map = self.director.storage.get("map", "city")
         self.music.loop_background_effect(
             [
@@ -93,6 +120,12 @@ class GamePlay(Level):
         return super().setup()
 
     def update(self):
+        """
+        Update the game state. Check the number of enemies left. If there are enemies remaining, adjust the background effects loop based on the number of enemies. If there is only one enemy left, set the background effects loop to 1 channel. Otherwise, set it to the specified number of channels. Start playing a looped background effect with a list of effect names. If there are no enemies left, clear the background effects loop. Finally, update the music.
+        """
+        """
+        Update the game state. Check the number of enemies left. If there are enemies remaining, adjust the background effects loop based on the number of enemies. If there is only one enemy left, set the background effects loop to 1 channel. Otherwise, set it to the specified number of channels. Start playing a looped background effect with a list of effect names. If there are no enemies left, clear the background effects loop. Finally, update the music.
+        """
         left_enemies = len(self.EnemyGroup.sprites())
         if left_enemies > 0:
             if left_enemies == 1:
@@ -116,15 +149,33 @@ class GamePlay(Level):
 
 
 class Pause(MenuScene):
+    """
+    A class representing the pause menu scene in a game.
+    """
     background = pygame.image.load("assets/menus/pause.png")
+
     def __init__(self, director):
+        """
+        Initialize an instance of the class with a director object.
+        @param director - the director object
+        @return None
+        """
         super().__init__(director)
 
     def setup(self):
+        """
+        Set up the UI group for the menu. The menu consists of two buttons: "Continue" and "Quit".
+        - The "Continue" button has two images for normal and hover states, and when clicked, it calls the `return_to_game` callback function. It is scaled to 0.7.
+        - The "Quit" button also has two images for normal and hover states, and when clicked, it calls the `sys.exit` function to exit the program. It is scaled to 0.56.
+        - The menu is positioned at coordinates (520, 380) and has a vertical spacing of 20 between the buttons.
+        """
         self.menu = UIGroup(
             [
                 Button(
-                    path=["assets/menus/continue.png", "assets/menus/continue_hover.png"],
+                    path=[
+                        "assets/menus/continue.png",
+                        "assets/menus/continue_hover.png",
+                    ],
                     callback=self.return_to_game,
                     scale=0.7,
                 ),
@@ -146,38 +197,83 @@ class Pause(MenuScene):
         self.menu.stack_vertical()
 
     def back_to_main_menu(self):
+        """
+        Return to the main menu by setting the current scene to the MainMenu scene.
+        @param self - the current object
+        @return None
+        """
         self.director.set_scene(MainMenu(self.director))
 
     def return_to_game(self):
+        """
+        Return to the game scene by setting the current scene of the director to the game scene.
+        """
         self.director.set_scene(self.director._game)
 
 
-
-
 class Intro(Scene):
+    """
+    A class representing an introductory scene in a video game or animation.
+    @param director - the director controlling the scene
+    @method setup - sets up the scene by initializing a video and setting its size
+    @method update - updates the scene, currently checks if the video is active
+    @method skip - allows the user to skip the intro by seeking to the next key moment in the video
+    @return None
+    """
     def __init__(self, director):
+        """
+        Initialize an instance of the class with a director object.
+        @param director - the director object
+        @return None
+        """
         super().__init__(director)
 
     def setup(self):
+        """
+        Set up the video by creating a Video object with the specified video file path. Set the size of the video to match the width and height of the director.
+        @return None
+        """
         self.video = Video("assets/intro.mp4")
         self.video.set_size((self.director.width, self.director.height))
 
     def update(self):
+        """
+        Check if the video is active. If it is not active, call the `finish()` method.
+        @return None
+        """
         if self.video.active == False:
             self.finish()
 
     def event(self, event):
+        """
+        Handle a specific event in a Pygame application. If the event is a key press event and the key pressed is the spacebar, call the `skip()` method.
+        @param event - the event to handle
+        """
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             self.skip()
 
     def draw(self, screen, window_scale):
+        """
+        Draw the video on the screen at the specified position.
+        @param screen - the screen to draw on
+        @param window_scale - the scale of the window
+        @return None
+        """
         self.video.draw(screen, (0, 0))
 
     def finish(self):
+        """
+        Finish the current scene and set the scene to the main menu. Close the video.
+        @return None
+        """
         self.director.set_scene(MainMenu(self.director))
         self.video.close()
 
     def skip(self):
+        """
+        Skip to the next key moment in the video playback.
+        @return None
+        """
         key_moments = [6, 13.9]
         current_time = self.video.get_playback_data()["time"]
         if current_time > key_moments[-1]:
@@ -190,12 +286,24 @@ class Intro(Scene):
 
 
 class MainMenu(MenuScene):
+    """
+    A class representing the main menu scene in a game. Inherits from the MenuScene class.
+    """
     background = pygame.image.load("assets/menus/main_menu.png")
 
     def __init__(self, director):
+        """
+        Initialize an instance of the class with a director object.
+        @param director - the director object
+        @return None
+        """
         super().__init__(director)
 
     def setup(self):
+        """
+        Set up the user interface for the game.
+        @return None
+        """
         self.menu = UIGroup(
             [
                 Button(
@@ -216,16 +324,38 @@ class MainMenu(MenuScene):
         self.menu.stack_vertical()
 
     def map_menu(self):
+        """
+        Display the map menu by setting the current scene to the SelectMap scene.
+        @return None
+        """
         self.director.set_scene(SelectMap(self.director), pause_music=False)
 
 
 class SelectMap(MenuScene):
+    """
+    A scene for selecting a map in a game menu.
+    @param MenuScene - The base class for the menu scene.
+    @param background - The background image for the scene.
+    @param director - The game director.
+    @method __init__ - Initializes the SelectMap scene.
+    @method setup - Sets up the menu with buttons for map selection.
+    @method set_map - Sets the selected map in the game storage.
+    """
     background = pygame.image.load("assets/menus/map_menu.png")
 
     def __init__(self, director):
+        """
+        Initialize an instance of the class with a director object.
+        @param director - the director object
+        @return None
+        """
         super().__init__(director)
 
     def setup(self):
+        """
+        Set up the menu for the game.
+        @return None
+        """
         self.menu = UIGroup(
             [
                 Button(
@@ -247,17 +377,39 @@ class SelectMap(MenuScene):
         self.menu.stack_horizontal()
 
     def set_map(self, map):
+        """
+        Set the map for the game by storing it in the storage and then setting the scene to the SelectLevel scene.
+        @param map - the map to be set
+        @return None
+        """
         self.director.storage.set("map", map)
         self.director.set_scene(SelectLevel(self.director), flush_music=True)
 
 
 class SelectLevel(MenuScene):
+    """
+    A scene for selecting a level in a game menu.
+    @param MenuScene - The base class for the menu scene.
+    @param background - The background image for the level menu.
+    @param director - The game director.
+    @method __init__ - Initializes the SelectLevel scene.
+    @method setup - Sets up the level menu by creating buttons and arranging them in a menu group.
+    @method set_level - Sets the selected level in the game storage.
+    """
     background = pygame.image.load("assets/menus/level_menu.png")
 
     def __init__(self, director):
+        """
+        Initialize an instance of the class with a director object.
+        @param director - the director object
+        @return None
+        """
         super().__init__(director)
 
     def setup(self):
+        """
+        Set up the menu for the game. The menu consists of multiple buttons, each with its own image, hover image, callback function, and scale. The buttons are arranged horizontally with a spacing of 20 pixels. The menu is positioned vertically at y=200. The menu is then centered horizontally on the screen.
+        """
         self.menu = UIGroup(
             [
                 Button(
@@ -284,17 +436,33 @@ class SelectLevel(MenuScene):
         self.menu.stack_horizontal()
 
     def set_level(self, level):
+        """
+        Set the level of the game to the specified level.
+        @param level - the level to set
+        @return None
+        """
         self.director.storage.set("level", level)
         self.director.set_scene(PerkMenu(self.director))
 
 
 class PerkMenu(MenuScene):
+    """
+    A class representing a menu scene for perks in a game.
+    """
     background = pygame.image.load("assets/menus/perks_menu.png")
 
     def __init__(self, director):
+        """
+        Initialize an instance of the class with a director object.
+        @param director - the director object
+        @return None
+        """
         super().__init__(director)
 
     def setup(self):
+        """
+        Set up the UI menu for the game. This includes creating a `UIGroup` object and adding `Button` objects to it. The buttons represent different perks that can be purchased in the game. The `path` parameter of each button specifies the image file paths for the button's normal and hover states. The `callback` parameter specifies the function to be called when the button is clicked. The `scale` parameter specifies the size of the button.
+        """
         self.menu = UIGroup(
             [
                 Button(
@@ -351,6 +519,12 @@ class PerkMenu(MenuScene):
         buttons.stack_vertical()
 
     def buy_perk(self, perk):
+        """
+        Buy a perk for the player.
+        @param self - the instance of the class
+        @param perk - the perk to buy
+        @return None
+        """
         price = 100
         amount = self.director.storage.get(perk, 0)
         money = self.director.storage.get("money", 0)
@@ -360,13 +534,26 @@ class PerkMenu(MenuScene):
             self.music.play_sound_effect("purchase")
 
     def continue_the_gameplay(self):
+        """
+        Continue the gameplay by setting the current scene to a new instance of the GamePlay scene.
+        @return None
+        """
         self.director.set_scene(GamePlay(self.director))
 
     def go_back_to_select_level(self):
+        """
+        Go back to the select level scene by setting the current scene of the director to the SelectLevel scene.
+        @param self - the current instance of the class
+        @return None
+        """
         self.director.set_scene(SelectLevel(self.director))
 
 
 def main():
+    """
+    The main function initializes the Pygame library, creates a Director object, sets the initial scene to Intro, sets up the director, starts the game loop, and cleans up resources before exiting the program.
+    @return None
+    """
     pygame.init()
     director = Director()
     director.set_scene(Intro(director))
@@ -374,8 +561,6 @@ def main():
     director.loop()
     pygame.quit()
     sys.exit()
-
-
 
 
 if __name__ == "__main__":
